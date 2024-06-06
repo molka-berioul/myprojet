1bf9862e4c685ec429e98197e549e18d2a38a332
malak <malak.berioul@gmail.com>
2024-06-06 20:06:57 +0100

    git hub

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/general/home/home.component';
+import { InscriptionComponent } from './modules/general/inscription/inscription.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : '',
+    component:HomeComponent,
+  },
+  {
+    path:'inscription',
+    component:InscriptionComponent,
+  },
+  
+  {
+    path : 'navbar',
+    component:NavbarComponent,
+  },
+  {
+    path : 'footer',
+    component:FooterComponent,
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/general/home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InscriptionComponent } from './modules/general/inscription/inscription.component';
+import { FooterComponent } from './footer/footer.component';
+
+import { registerLocaleData } from '@angular/common';
+import *as fr from '@angular/common/locales/fr';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    InscriptionComponent,
+    
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID ,useValue:'fr-FR'}
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){
+    registerLocaleData(fr.default);
+  }
+ }
